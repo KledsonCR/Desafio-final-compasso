7409f5d51c1f408f03e674ac922e43aea174d743
kledson costa <kledsoncrabelo@icloud.com>
2022-06-03 11:51:58 -0300

    feat:  add CarControler in controller layer

---
+++ src/app/controllers/CarController.js
@@ -0,0 +1,14 @@
+const CarService = require('../service/CarService')
+
+class CarController {
+    create(req, res) {
+        try{
+          const result = CarService.create(req.body)
+          return res.status(201).json(result)
+        }catch(error){
+          return res.status(400).json(error)
+        }
+    }
+}
+
+module.exports = new CarController()
