475456c1851368ea54b3dc427cd2d1a056bac917
kledson costa <kledsoncrabelo@icloud.com>
2022-06-03 12:03:03 -0300

    feat: add create method in repository and service layer

---
+++ src/app/repository/CarRepository.js
@@ -0,0 +1,9 @@
+const CarSchema = require('../schema/CarSchema')
+
+class CarRepository {
+    create(payload) {
+        return CarSchema.create(payload)
+    }
+}
+
+module.exports = new CarRepository()
---
+++ src/app/service/CarService.js
@@ -0,0 +1,10 @@
+const CarRepository = require('../repository/CarRepository')
+
+class CarService {
+    create(payload) {
+        const result = CarRepository.create(payload)
+        return result
+    }
+}
+
+module.exports = new CarService()
