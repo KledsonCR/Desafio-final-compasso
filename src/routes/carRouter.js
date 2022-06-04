b1dd9374261cd98752c69da30a71ed8c01df46a6
kledson costa <kledsoncrabelo@icloud.com>
2022-06-04 20:55:38 -0300

    feat: add get byid route

--- src/routes/carRouter.js
+++ src/routes/carRouter.js
@@ -3,6 +3,8 @@ const CarController = require('../app/controllers/CarController');
 module.exports = (server, routes, prefix = '/api/v1/car') => {
 	routes.post('/', CarController.create);
 	routes.get('/', CarController.find);
+	routes.get('/:id', CarController.findById);
+
 
 	server.use(prefix, routes);
 };
