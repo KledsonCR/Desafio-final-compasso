43ea976c63211cc0382b3c9f895b2ef2bcc1fc37
kledson costa <kledsoncrabelo@icloud.com>
2022-06-05 19:16:13 -0300

    feat: add validation in post route

--- src/routes/carRouter.js
+++ src/routes/carRouter.js
@@ -1,7 +1,8 @@
 const CarController = require('../app/controllers/CarController');
+const createValidation = require('../app/validation/car/create');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-	routes.post('/', CarController.create);
+	routes.post('/', createValidation, CarController.create);
 	routes.get('/', CarController.find);
 	routes.get('/:id', CarController.findById);
 	routes.put('/:id', CarController.update);
