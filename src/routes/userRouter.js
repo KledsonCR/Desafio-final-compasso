63e44415d1cbe9a9dcf013facf263c7cc0110237
kledson costa <kledsoncrabelo@icloud.com>
2022-06-05 09:50:30 -0300

    feat: add get route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ const UserController = require('../app/controllers/UserController');
 
 module.exports = (server, routes, prefix = '/api/v1/person') => {
 	routes.post('/', UserController.create);
+	routes.get('/', UserController.findAll);
 
 	server.use(prefix, routes);
 };
