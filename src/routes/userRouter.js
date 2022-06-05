adfaed74cf5e1a1cc78c4ed814eb6d2cef3aed5b
kledson costa <kledsoncrabelo@icloud.com>
2022-06-05 10:23:37 -0300

    feat: add put and delete routes for users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,6 +4,8 @@ module.exports = (server, routes, prefix = '/api/v1/person') => {
 	routes.post('/', UserController.create);
 	routes.get('/', UserController.findAll);
 	routes.get('/:id', UserController.findById);
+	routes.put('/:id', UserController.update);
+	routes.delete('/:id', UserController.delete);
 
 	server.use(prefix, routes);
 };
