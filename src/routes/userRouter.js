1609b7b5d8bb325da70235ae3fa2559c4b63ef48
kledson costa <kledsoncrabelo@icloud.com>
2022-06-05 10:02:16 -0300

    feat: add get route with findById for users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,7 @@ const UserController = require('../app/controllers/UserController');
 module.exports = (server, routes, prefix = '/api/v1/person') => {
 	routes.post('/', UserController.create);
 	routes.get('/', UserController.findAll);
+	routes.get('/:id', UserController.findById);
 
 	server.use(prefix, routes);
 };
