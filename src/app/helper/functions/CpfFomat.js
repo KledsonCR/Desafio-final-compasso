ff6eb84d999a1bd992b911e2c0eacb0237eccf32
kledson costa <kledsoncrabelo@icloud.com>
2022-06-07 13:08:33 -0300

    fix: fixed bug of this function

--- src/app/helper/functions/CpfFomat.js
+++ src/app/helper/functions/CpfFomat.js
@@ -1,5 +1,5 @@
 function cpfFormat(Cpf) {	
-	const cpf = Cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+	const cpf = Cpf.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 	const users = Object.assign(Cpf, { cpf });
 	return users;
 }
