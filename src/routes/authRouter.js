390a570bfe1b0a7f83ac41612cf7b003ab8387c9
kledson costa <kledsoncrabelo@icloud.com>
2022-06-09 11:22:23 -0300

    style: rename to authentica

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,7 @@
-const AutheController = require('../app/controllers/AutheController');
+const AuthenticaController = require('../app/controllers/AuthenticaController');
 
 module.exports = (server, routes, prefix = '/api/v1/authenticate') => {
-	routes.post('/', AutheController.authenticate);
+	routes.post('/', AuthenticaController.authenticate);
 
 	server.use(prefix, routes);
 };
