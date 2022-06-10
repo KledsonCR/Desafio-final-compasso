0777f18abb736475459ffa2ef7478ea20beec1a2
kledson costa <kledsoncrabelo@icloud.com>
2022-06-10 12:40:08 -0300

    feat: add rental route post

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 const Car = require('./carRouter');
 const User = require('./userRouter');
 const Auth = require('./authRouter');
+const Rental = require('./rentalRouter');
 
 module.exports = (server) => {
 	server.use((req, res, next) => {
 		Car(server, new Router());
 		User(server, new Router());
 		Auth(server, new Router());
+		Rental(server, new Router());
 
 		next();
 	});
