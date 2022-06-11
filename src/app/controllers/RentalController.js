ef7635b85d1b59f84053e0c0ca1bf120e36880f8
kledson costa <kledsoncrabelo@icloud.com>
2022-06-11 12:27:06 -0300

    feat: add search methods to layers

--- src/app/controllers/RentalController.js
+++ src/app/controllers/RentalController.js
@@ -10,6 +10,16 @@ class RentalController {
 			return res.status(500).json(error);		
 		}
 	}
+	async findAll(req, res) {
+		const payload = req.query;
+		try {
+			const result = await RentalService.findAll(payload);
+			return res.status(200).json(result);
+		}catch(error){
+			return res.status(500).json(error);
+		}
+	}
 }
+
 
 module.exports = new RentalController();
--- src/app/repository/RentalRepository.js
+++ src/app/repository/RentalRepository.js
@@ -4,6 +4,10 @@ class RentalRepository {
 	async create(payload) {
 		return RentalSchema.create(payload);
 	}
+
+	async findAll(payload) {
+		return RentalSchema.find(payload);
+	}
 }
 
 module.exports = new RentalRepository();
