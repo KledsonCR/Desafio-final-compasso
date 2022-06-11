3ef7721a3848159a214bea087a14cb42c922c8c5
kledson costa <kledsoncrabelo@icloud.com>
2022-06-11 13:06:51 -0300

    feat: add put and delete method in rental route

--- src/routes/rentalRouter.js
+++ src/routes/rentalRouter.js
@@ -4,6 +4,8 @@ module.exports = (server, routes, prefix = '/api/v1/rental') => {
 	routes.post('/', RentalController.create);
 	routes.get('/', RentalController.findAll);
 	routes.get('/:id', RentalController.findById);
+	routes.put('/:id', RentalController.update);
+	routes.delete('/:id', RentalController.delete);
 
 
 	server.use(prefix, routes);
