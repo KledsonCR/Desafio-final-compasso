6020fea353110330d346b0a0155bc9b1eedf56f3
kledson costa <kledsoncrabelo@icloud.com>
2022-06-11 12:48:36 -0300

    feat: add get findById method in rental route

--- src/routes/rentalRouter.js
+++ src/routes/rentalRouter.js
@@ -3,6 +3,7 @@ const RentalController = require('../app/controllers/RentalController');
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
 	routes.post('/', RentalController.create);
 	routes.get('/', RentalController.findAll);
+	routes.get('/:id', RentalController.findById);
 
 
 	server.use(prefix, routes);
