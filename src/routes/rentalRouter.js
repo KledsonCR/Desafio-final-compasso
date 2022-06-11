b6aa998d16874cb37404a259a5db447252126df5
kledson costa <kledsoncrabelo@icloud.com>
2022-06-11 12:28:11 -0300

    feat: add get route

--- src/routes/rentalRouter.js
+++ src/routes/rentalRouter.js
@@ -2,6 +2,8 @@ const RentalController = require('../app/controllers/RentalController');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
 	routes.post('/', RentalController.create);
+	routes.get('/', RentalController.findAll);
+
 
 	server.use(prefix, routes);
 };
