0a734d2360814f6cb93d2bdc85cf23203a42a5a1
kledson costa <kledsoncrabelo@icloud.com>
2022-06-15 10:07:58 -0300

    feat: add swagger router

---
+++ src/routes/docs/swaggerRouter.js
@@ -0,0 +1,11 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../../../swagger.json');
+
+module.exports = (server, routes, prefix = '/api/v1/api-docs') => {
+	routes.use('/', swaggerUi.serve); 
+	routes.get('/', swaggerUi.setup(swaggerDocs));
+	
+	server.use(prefix, routes);
+};
+
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const Car = require('./carRouter');
 const User = require('./userRouter');
 const Auth = require('./authRouter');
 const Rental = require('./rentalRouter');
+const Swagger = require('./docs/swaggerRouter');
 
 module.exports = (server) => {
 	server.use((req, res, next) => {
@@ -10,6 +11,7 @@ module.exports = (server) => {
 		User(server, new Router());
 		Auth(server, new Router());
 		Rental(server, new Router());
+		Swagger(server, new Router());
 
 		next();
 	});
