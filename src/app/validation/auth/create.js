ecf3c78a8a733a5a56b5f337e5e9b806e82a0b28
kledson costa <kledsoncrabelo@icloud.com>
2022-06-22 04:29:00 -0300

    refactor: improving error handling in joi with map

--- src/app/validation/auth/create.js
+++ src/app/validation/auth/create.js
@@ -3,24 +3,19 @@ const Joi = require('joi').extend(require('@joi/date'));
 module.exports = async (req, res, next) => {
 	try {
 		const schema = Joi.object({
-			email: Joi.string().trim().required(),           
-			password: Joi.string().min(6).required(), 
+			email: Joi.string().trim().email().required(),
+			password: Joi.string().min(6).required(),
 		});
 
 		const { error } = await schema.validate(req.body, { abortEarly: false });
-		if (error) {
-			throw {
-				message: 'Ba Request',
-				details: [
-					{
-						message: error.message,
-					},
-				],
-			};
-		}
-
+		if (error) throw error;
 		return next();
 	} catch (error) {
-		return res.status(400).json(error);
+		return res.status(400).json({
+			errors: error.details.map((detail) => ({
+				name: detail.path.join('.'),
+				description: detail.message,
+			})),
+		});
 	}
 };
