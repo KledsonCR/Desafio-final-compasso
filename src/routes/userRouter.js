2d7a575026cd1589d48d8f3f61e5f359c71d0d2e
kledson costa <kledsoncrabelo@icloud.com>
2022-06-22 05:05:19 -0300

    fix: fixing validation import

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,8 @@
 const UserController = require('../app/controllers/UserController');
 const createValidation = require('../app/validation/user/create');
 const updateValidation = require('../app/validation/user/update');
-const getValidation = require('../app/validation/car/get');
-const deleteValidation = require('../app/validation/car/delete');
+const getValidation = require('../app/validation/user/get');
+const deleteValidation = require('../app/validation/user/delete');
 
 module.exports = (server, routes, prefix = '/api/v1/person') => {
 	routes.post('/', createValidation, UserController.create);
