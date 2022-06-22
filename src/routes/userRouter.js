c270b02ea9b3cd4add52969dce5a53acf13ba2ca
kledson costa <kledsoncrabelo@icloud.com>
2022-06-22 04:37:22 -0300

    feat: add validation paths to car and user routes

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,12 +1,15 @@
 const UserController = require('../app/controllers/UserController');
 const createValidation = require('../app/validation/user/create');
+const updateValidation = require('../app/validation/user/update');
+const getValidation = require('../app/validation/car/get');
+const deleteValidation = require('../app/validation/car/delete');
 
 module.exports = (server, routes, prefix = '/api/v1/person') => {
 	routes.post('/', createValidation, UserController.create);
-	routes.get('/', UserController.findAll);
+	routes.get('/', getValidation, UserController.findAll);
 	routes.get('/:id', UserController.findById);
-	routes.put('/:id', UserController.update);
-	routes.delete('/:id', UserController.delete);
+	routes.put('/:id', updateValidation, UserController.update);
+	routes.delete('/:id', deleteValidation, UserController.delete);
 
 	server.use(prefix, routes);
 };
