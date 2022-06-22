8cd38704e54c81e85a1cd1d18e6afdf3b0e21d47
kledson costa <kledsoncrabelo@icloud.com>
2022-06-22 07:48:06 -0300

    feat: add reserve router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const Car = require('./carRouter');
 const User = require('./userRouter');
 const Auth = require('./authRouter');
 const Rental = require('./rentalRouter');
+const Reserve = require('./reserveRouter');
 const Swagger = require('./docs/swaggerRouter');
 
 module.exports = (server) => {
@@ -11,6 +12,7 @@ module.exports = (server) => {
 		User(server, new Router());
 		Auth(server, new Router());
 		Rental(server, new Router());
+		Reserve(server, new Router());
 		Swagger(server, new Router());
 
 		next();
