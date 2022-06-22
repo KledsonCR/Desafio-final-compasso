fe928c4aef1015557a405b7dfbb241f9c29f6272
kledson costa <kledsoncrabelo@icloud.com>
2022-06-22 04:31:15 -0300

    refactor: improving error handling in joi with map and correcting flag

--- src/app/validation/car/create.js
+++ src/app/validation/car/create.js
@@ -7,30 +7,29 @@ module.exports = async (req, res, next) => {
 			type: Joi.string().trim().required(),
 			brand: Joi.string().trim().required(),
 			color: Joi.string().trim().required(),           
-			year: Joi.number().min(1950).max(2022),
+			year: Joi.number().min(1950).max(2022).required(), 
 			accessories: Joi.array()
 				.unique()
 				.min(1)
-				.items(Joi.object({ description: Joi.string().trim().required() }).min(1))	
+				.items({ description: Joi.string().trim().required() })
+				.min(1)
 				.required(),
-			passengersQtd: Joi.number().integer().min(1).max(5).required()
-
+			passengersQtd: Joi.number().integer()
+				.min(1)
+				.max(5)
+				.required()
 		});
 
-		const { error } = await schema.validate(req.body, { abortEarl: true });
-		if (error) {
-			throw {
-				message: 'Bad Request',
-				details: [
-					{
-						message: error.message,
-					},
-				],
-			};
-		}
+		const { error } = await schema.validate(req.body, { abortEarly: false });
+		if (error) throw error;
 
 		return next();
 	} catch (error) {
-		return res.status(400).json(error);
+		return res.status(400).json({
+			errors: error.details.map((detail) => ({
+				name: detail.path.join('.'),
+				description: detail.message
+			}))
+		});
 	}
 };
