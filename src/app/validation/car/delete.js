266c2b4af40de7aeb4e528dda43275d7cb2cac07
kledson costa <kledsoncrabelo@icloud.com>
2022-06-22 04:35:30 -0300

    feat: add validations in update, delete and get for cars

---
+++ src/app/validation/car/delete.js
@@ -0,0 +1,33 @@
+const Joi = require('joi').extend(require('@joi/date'));
+
+module.exports = async (req, res, next) => {
+	try {
+		const schema = Joi.object({
+			model: Joi.string().trim(),
+			type: Joi.string().trim(),
+			brand: Joi.string().trim(),
+			color: Joi.string().trim(),           
+			year: Joi.number().min(1950).max(2022), 
+			accessories: Joi.array()
+				.unique()
+				.min(1)
+				.items({ description: Joi.string().trim()})
+				.min(1),
+			passengersQtd: Joi.number().integer()
+				.min(1)
+				.max(5)				
+		});
+
+		const { error } = await schema.validate(req.body, { abortEarly: false });
+		if (error) throw error;
+
+		return next();
+	} catch (error) {
+		return res.status(400).json({
+			errors: error.details.map((detail) => ({
+				name: detail.path.join('.'),
+				description: detail.message
+			}))
+		});
+	}
+};
