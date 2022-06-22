e253eb3deab75d66d6314b139ccd3d979c7d1a66
kledson costa <kledsoncrabelo@icloud.com>
2022-06-22 04:33:29 -0300

    refactor: improving error handling in joi with map and adding regex for cpf

--- src/app/validation/user/create.js
+++ src/app/validation/user/create.js
@@ -1,42 +1,33 @@
 const Joi = require('joi').extend(require('@joi/date'));
-const validateCpf = require('../../helper/functions/CpfValid');
+const { cpfRegex } = require('../../helper/utils/Regex');
 
 const birthDay = new Date(Date.now() - 1000 * 60 * 60 * 24 * 365 * 18);
 
 module.exports = async (req, res, next) => {
 	try {
 		const schema = Joi.object({
-			name: Joi.string().min(3).required(),
-			cpf: Joi.string()
-				.min(11).max(14)
-				.custom((value, help) => {
-					if (!validateCpf(value)) {
-						return help.message('Invalid CPF!');
-					}
-					return true;
-				})
-				.required(),
+			name: Joi.string().min(3).trim().required(),
+			cpf: Joi.string().regex(cpfRegex())
+				.message(
+					'Cpf with invalid format, use only xxx.xxx.xxx-xx format!'
+				)
+				.required(),			
 			birthDay: Joi.date().format().max(birthDay).required(),
-			email: Joi.string().trim().required(),           
-			password: Joi.string().min(6),
-			canDrive: Joi.string().required().valid('yes', 'no').required(),
-
+			email: Joi.string().trim().email().required(),           
+			password: Joi.string().min(6).required(),
+			canDrive: Joi.string().valid('yes', 'no').required(),
 		});
 
-		const { error } = await schema.validate(req.body, { abortEarl: true });
-		if (error) {
-			throw {
-				message: 'Bad Request',
-				details: [
-					{
-						message: error.message,
-					},
-				],
-			};
-		}
+		const { error } = await schema.validate(req.body, { abortEarly: false });
+		if (error) throw error;
 
 		return next();
 	} catch (error) {
-		return res.status(400).json(error);
+		return res.status(400).json({
+			errors: error.details.map((detail) => ({
+				name: detail.path.join('.'),
+				description: detail.message
+			}))
+		});
 	}
 };
