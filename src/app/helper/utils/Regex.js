af98fa796c4625d82db62bca69d6338d2366bf2f
kledson costa <kledsoncrabelo@icloud.com>
2022-06-26 09:50:42 -0300

    feat: add cep regex

--- src/app/helper/utils/Regex.js
+++ src/app/helper/utils/Regex.js
@@ -2,7 +2,10 @@ const cpfRegex = () => /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
 
 const cnpjRegex = () => /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
 
+const cepRegex = () => /^[0-9]{5}-[0-9]{3}$/;
+
 module.exports = {
   cpfRegex,
-  cnpjRegex
+  cnpjRegex,
+  cepRegex
 };
