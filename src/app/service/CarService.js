02f0a25c5db67b4ff8be568e528f3481313ffc78
kledson costa <kledsoncrabelo@icloud.com>
2022-06-26 20:46:44 -0300

    feat: add error handling

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -1,37 +1,41 @@
 const CarRepository = require('../repository/CarRepository');
 const NotFound = require('../errors/NotFound');
+const BadRequest = require('../errors/BadRequest');
 
 class CarService {
   async create(payload) {
     const result = await CarRepository.create(payload);
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async findAll(payload) {
     const result = await CarRepository.findAll(payload);
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async findById(id) {
     const result = await CarRepository.findById(id);
-    if (result == null) throw new NotFound(id);
+    if (!result) throw new BadRequest(`Id ${id} It is not valid`);
     return result;
   }
 
   async update(id, payload) {
     const result = await CarRepository.update(id, payload);
-    if (result == null) throw new NotFound(id);
+    if (!result) throw new BadRequest(`Id ${id} It is not valid`);
     return result;
   }
 
   async delete(id) {
     const result = await CarRepository.delete(id);
-    if (result == null) throw new NotFound(id);
+    if (!result) throw new NotFound(`Id ${id} It is not valid`);
     return result;
   }
 
   async updateAccessories(id, descriptionId, payload) {
     const result = await CarRepository.updateAccessories(id, descriptionId, payload);
+    if (!result) throw new NotFound(`Id ${id} It is not valid`);
     return result;
   }
 }
