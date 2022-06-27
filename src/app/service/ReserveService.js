7e4b6613ff915c8a332b2b08f545bbf709440efc
kledson costa <kledsoncrabelo@icloud.com>
2022-06-26 21:13:39 -0300

    feat: add error handling reserve

--- src/app/service/ReserveService.js
+++ src/app/service/ReserveService.js
@@ -1,28 +1,34 @@
 const ReserveRepository = require('../repository/ReserveRepository');
+const NotFound = require('../errors/NotFound');
 
 class ReserveService {
   async create(id, payload) {
     const result = await ReserveRepository.create({ ...id, ...payload });
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async findAll(payload) {
     const result = await ReserveRepository.findAll(payload);
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async findById(id) {
     const result = await ReserveRepository.findById(id);
+    if (!result) throw new NotFound(`Id ${id} It is not valid`);
     return result;
   }
 
   async update(id, payload) {
     const result = await ReserveRepository.update(id, payload);
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async delete(id) {
     const result = await ReserveRepository.delete(id);
+    if (!result) throw new NotFound(`Id ${id} It is not valid`);
     return result;
   }
 }
