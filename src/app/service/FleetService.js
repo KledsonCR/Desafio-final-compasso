bd1f0c5306938c22a685bd97064877852a6786f3
kledson costa <kledsoncrabelo@icloud.com>
2022-06-26 21:13:05 -0300

    feat: add error handling fleet

--- src/app/service/FleetService.js
+++ src/app/service/FleetService.js
@@ -1,28 +1,34 @@
 const FleetRepository = require('../repository/FleetRepository');
+const NotFound = require('../errors/NotFound');
 
 class FleetService {
   async create(id, payload) {
     const result = await FleetRepository.create({ ...id, ...payload });
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async findAll(payload) {
     const result = await FleetRepository.findAll(payload);
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async findById(id) {
     const result = await FleetRepository.findById(id);
+    if (!result) throw new NotFound(`Id ${id} It is not valid`);
     return result;
   }
 
   async update(id, payload) {
     const result = await FleetRepository.update(id, payload);
+    if (!result) throw new NotFound('Not found');
     return result;
   }
 
   async delete(id) {
     const result = await FleetRepository.delete(id);
+    if (!result) throw new NotFound(`Id ${id} It is not valid`);
     return result;
   }
 }
